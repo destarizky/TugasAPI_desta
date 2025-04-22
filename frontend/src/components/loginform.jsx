d6a2341833e28166c89d7ce659d3496c1e93c14b
Desta Rizky <destarizkyandika24@gmail.com>
2025-04-22 12:23:43 +0700

    update

--- frontend/src/components/loginform.jsx
+++ frontend/src/components/loginform.jsx
@@ -12,7 +12,7 @@ const LoginForm = () => {
     setClicked(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch('"http://localhost:5000/api/auth/login"', {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
